3f98dcce1de0819bd33109e1da6339814f2e63c8
root <root@srv715439.hstgr.cloud>
2025-04-03 00:22:44 +0000

    atualizando-clenteDTO

--- src/clientes/dto/create-cliente.dto.ts
+++ src/clientes/dto/create-cliente.dto.ts
@@ -2,9 +2,12 @@ import { IsNotEmpty, Matches } from 'class-validator';
 
 export class CreateClienteDto {
   @IsNotEmpty()
-  @Matches(/^PCN\d{4}$/, { message: 'O ID do cliente deve seguir o formato PCNXXXX (ex: PCN2300)' })
+  @Matches(/^[A-Z]{3}\d{4}$/, {
+    message: 'O ID do cliente deve seguir o formato AAA9999 (3 letras + 4 dígitos, ex: PCN2300 ou ARV0052)',
+  })
   id: string;
 
   @IsNotEmpty()
   nome: string;
 }
+
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    /* origin: 'http://localhost:3001', */
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
