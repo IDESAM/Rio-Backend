187767c22734760a186b02c951b559ab625ba0a0
unknown <mateusvpdl@outlook.com>
2025-05-20 22:34:22 -0300

    adicionando metodo para verificar saf, proprietario e comunidade já cadastrados

--- src/plantios/dto/create-plantio.dto.ts
+++ src/plantios/dto/create-plantio.dto.ts
@@ -2,9 +2,6 @@ import { IsNotEmpty, IsNumber, Matches } from 'class-validator';
 
 export class CreatePlantioDto {
   @IsNotEmpty()
-  @Matches(/^[A-Z]{3}\d{4}$/, {
-    message: 'O ID do cliente deve seguir o formato AAA9999 (3 letras + 4 dígitos, ex: PCN2300 ou ARV0052)',
-  })
   clienteId: string;
 
   @IsNotEmpty()
