6b29be70ff53d3c329c392fcd32883690c38f1ba
Seu Nome <seu-email@exemplo.com>
2026-03-20 17:26:23 -0300

    chore: padronizando para commonjs

--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import 'dotenv/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
@@ -8,10 +9,10 @@ const dataSourceOptions: DataSourceOptions = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    migrations: [__dirname + '/migrations/*.{js,ts}']
+    // No CommonJS, o TypeORM entende caminhos relativos com __dirname facilmente
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/migrations/*{.ts,.js}'],
+    logging: true,
 };
 
-const dataSource = new DataSource(dataSourceOptions);
-
-export default dataSource;
+export const AppDataSource = new DataSource(dataSourceOptions);
